71adc41ac9792349dc437612dd345fe3780bc485
Dima Shirkov <dimashirkov@list.ru>
2022-06-25 17:08:09 +0300

    dak1 draft v2

--- task.js
+++ task.js
@@ -1,29 +1,60 @@
 const rotatorCase = Array.from(document.querySelectorAll('.rotator__case'));
 
-// for (i = 0; i < rotatorCase.length; i++) {
-//     setInterval(function(i) {
-//         let previousElement = rotatorCase[i].previousElementSibling;
-//         previousElement.classList.remove('rotator__case_active');
-//         rotatorCase[i].classList.add('rotator__case_active');
-//     }, 1000)
-// }
-
-
-
-// работает, но не возвращает первую строку и не начинает цикл заново
+// итерация непредсказуема, после i = 4, переходит на i = 0, не понимаю почему
 for (let i = 0; i < rotatorCase.length; i++) {
-    setTimeout(function (i) {
-        let previousElement = rotatorCase[i].previousElementSibling; // доступ к предыдущему элементу
-        let nextElement = rotatorCase[i].nextElementSibling; // доступ к следующему элементу
-        rotatorCase[i].classList.remove('rotator__case_active'); // у первого удалить класс
+
+    function round(i) {
+        let currentElement = rotatorCase[i]; // обрабатываемый элемент
+        let previousElement = rotatorCase[i].previousElementSibling; // предыдущий элемент
+        let nextElement = rotatorCase[i].nextElementSibling; // следующий элемент
+        // currentElement.style.color = currentElement.dataset.color; // смена цвета шрифта
 
         if (previousElement === null) { // если предыдущего нет
-            return nextElement.classList.add('rotator__case_active'); // к следущему добавить класс
-        } else if (nextElement === null) { // если нет следующего 
-            return rotatorCase[0].classList.add('rotator__case_active'); // добавить класс к первому элементу массива
+            // rotatorCase[rotatorCase.length - 1].classList.remove('rotator__case_active');
+            // currentElement.classList.add('rotator__case_active'); // добавить класс к обрабатываемому
+            return currentElement.style.color = currentElement.dataset.color; // смена цвета шрифта у обрабатываемого элемента
+            // return false;
+        // } else if (nextElement === null) { // если нет следующего 
+        //     previousElement.classList.remove('rotator__case_active'); // у предыдущего удалить класс
+        //     currentElement.classList.add('rotator__case_active'); // добавить класс к первому элементу массива 
+        //     return false;
         } else {
-            return nextElement.classList.add('rotator__case_active'); // добавить класс к следующему
+            previousElement.classList.remove('rotator__case_active'); // у предыдущего удалить класс
+            currentElement.classList.add('rotator__case_active'); // добавить класс к обрабатываемому
+            return currentElement.style.color = currentElement.dataset.color; // смена цвета шрифта у обрабатываемого элемента
+            // return false;
         };
-    }, 1000 * i, i);
-};
-//
+    };
+
+    setInterval(round, 1000 * i, i)
+}
+
+
+ // не работает повторение цикла
+
+// let roundAds = function() {
+//     for (let i = 0; i < rotatorCase.length; i++) {
+
+//         setTimeout(function (i) {
+//             let currentElement = rotatorCase[i]; // обрабатываемый элемент
+//             let previousElement = rotatorCase[i].previousElementSibling; // предыдущий элемент
+//             let nextElement = rotatorCase[i].nextElementSibling; // следующий элемент
+//             currentElement.style.color = currentElement.dataset.color; // изменение цвета шрифта
+            
+//             if (i === 0 & rotatorCase[rotatorCase.length - 1].classList.contains('rotator__case_active')) {
+//                 rotatorCase[rotatorCase.length - 1].classList.remove('rotator__case_active');
+//                 return rotatorCase[0].classList.add('rotator__case_active');
+//             } else if (previousElement === null) { // если предыдущего нет
+//                 return false;
+//             } else if (nextElement === null) { // если нет следующего 
+//                 previousElement.classList.remove('rotator__case_active'); // у предыдущего удалить класс
+//                 return currentElement.classList.add('rotator__case_active'); // добавить класс к первому элементу массива
+//             } else {
+//                 previousElement.classList.remove('rotator__case_active'); // у предыдущего удалить класс
+//                 return currentElement.classList.add('rotator__case_active'); // добавить класс к следующему
+//             };
+//         }, 1000 * i, i); // не понимаю почему мс нужно умножать на i? без этого не работает
+//     };
+// }
+
+// roundAds();
